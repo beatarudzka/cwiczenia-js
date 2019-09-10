e47484ba31ecefbd7fdd0bfda4c4a3d46790376b
Beata Rudzka <beatarudzka89@gmail.com>
2019-09-10 13:21:05 +0200

    creating and styling line

---
+++ task4/script.js
@@ -0,0 +1,15 @@
+const line = document.createElement('div')
+let size = 100
+
+line.style.height = size + "px"
+line.style.backgroundColor = "green"
+line.style.margin = "0"
+line.style.padding = "0"
+line.style.width = "100%"
+line.style.position = "fixed"
+line.style.left = "0%"
+line.style.top = "0%"
+
+document.body.appendChild(line)
+
+document.body.style.height = "100000px"
