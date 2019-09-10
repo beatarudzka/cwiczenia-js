9fd5757efdfc3c3bf3f1c6fec776e83d26c8cd9f
Beata Rudzka <beatarudzka89@gmail.com>
2019-09-10 11:27:16 +0200

    encreasing square by scrolling

--- task3/script.js
+++ task3/script.js
@@ -1,3 +1,12 @@
 const square = document.createElement('div')
-
+let size = 10
+square.style.height = size + "px"
+square.style.width = size + "px"
 document.body.appendChild(square)
+
+
+window.addEventListener("scroll", function () {
+  size++
+  square.style.width = size + "px"
+  square.style.height = size + "px"
+})
