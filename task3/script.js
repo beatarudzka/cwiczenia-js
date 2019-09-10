830ecda6d7c02d262eb83aca3b1213350cfcfbda
Beata Rudzka <beatarudzka89@gmail.com>
2019-09-10 12:45:03 +0200

    if statement for chanhing size of square

--- task3/script.js
+++ task3/script.js
@@ -4,9 +4,25 @@ square.style.height = size + "px"
 square.style.width = size + "px"
 document.body.appendChild(square)
 
+grow = true
+
 
 window.addEventListener("scroll", function () {
-  size++
-  square.style.width = size + "px"
-  square.style.height = size + "px"
+  if (size >= window.innerHeight / 2) {
+    grow = false
+  }
+  else if (size <= 0) {
+    grow = true
+  }
+
+  if (grow == true) {
+    size += 5
+    square.style.width = size + "px"
+    square.style.height = size + "px"
+  }
+  else {
+    size = size - 5
+    square.style.width = size + "px"
+    square.style.height = size + "px"
+  }
 })
