a21195b10961f9a36ede7dca53bd86e7c09a7fc3
Beata Rudzka <beatarudzka89@gmail.com>
2019-09-10 15:37:47 +0200

    changingSize function

--- task4/script.js
+++ task4/script.js
@@ -1,5 +1,6 @@
 const line = document.createElement('div')
 let size = 100
+let grow = true
 
 line.style.height = size + "px"
 line.style.backgroundColor = "green"
@@ -13,3 +14,28 @@ line.style.top = "0%"
 document.body.appendChild(line)
 
 document.body.style.height = "100000px"
+
+const changeHeight = function () {
+  console.log(size)
+  console.log(innerHeight)
+  console.log(grow)
+  if (size >= window.innerHeight / 2) {
+    grow = !grow
+  }
+  else if (size <= 0) {
+    grow = !grow
+  }
+
+  if (grow) {
+    size += 5
+    line.style.height = size + "px"
+    line.style.backgroundColor = "green"
+  }
+  else {
+    size -= 5
+    line.style.height = size + "px"
+    line.style.backgroundColor = "red"
+  }
+}
+
+window.addEventListener("scroll", changeHeight)
