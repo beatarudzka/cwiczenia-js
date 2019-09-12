839fae70e496ef5fb04b132acd154628df36e52a
Beata Rudzka <beatarudzka89@gmail.com>
2019-09-12 09:22:57 +0200

    changing colors

--- task7/script.js
+++ task7/script.js
@@ -1,5 +1,14 @@
 const title = document.querySelector('h1')
 
 document.body.addEventListener("mousemove", (event) => {
-  title.textContent = `${event.clientX}, ${event.clientY}`
+  const x = event.clientX + 1
+  const y = event.clientY + 1
+  const width = window.innerWidth
+  const height = window.innerHeight
+  title.textContent = x + ", " + y
+
+  const red = x / width * 100
+  const green = y / height * 100
+  const blue = ((x / width * 100) + (y / height * 100) / 2)
+  document.body.style.backgroundColor = `rgb(${red}%, ${green}%, ${blue}%)`
 })
