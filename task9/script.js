8eb755d59c34f080f90a5f870b2c660e66302ddd
Beata Rudzka <beatarudzka89@gmail.com>
2019-09-12 20:10:50 +0200

    WIP

--- task9/script.js
+++ task9/script.js
@@ -1,24 +1,31 @@
 square = document.querySelector('div')
 
-let divX = 150
-let divY = 50
+let divX
+let divY
+let drawActive = false
 
 square.style.left = `${divX}px`
 square.style.top = `${divY}px`
 
 square.addEventListener("mousedown", (event) => {
   square.style.backgroundColor = "grey"
+  drawActive = !drawActive
 
+  let insertDivX = event.offsetX
+  let insertDivY = event.offsetY
 })
 
 square.addEventListener("mouseup", (event) => {
   square.style.backgroundColor = "black"
+  drawActive = false
 })
 
 square.addEventListener("mousemove", (event) => {
-  const x = event.clientX
-  const y = event.clientY
-  square.style.left = `${x}px`
-  square.style.top = `${y}px`
+  if (drawActive) {
+    const x = event.clientX
+    const y = event.clientY
+    square.style.left = `${x - insertDivX}px`
+    square.style.top = `${y - insertDivY}px`
+  }
 
 })
