86b25c2668d7024df0ad79f343921b72a3bc73ad
Beata Rudzka <beatarudzka89@gmail.com>
2019-09-27 11:39:53 +0200

    add message when password is correct

---
+++ task10/index.js
@@ -0,0 +1,10 @@
+const input = document.querySelector('.password')
+const div = document.querySelector('.message')
+
+const password = '1234'
+const message = 'wyjechałam na zawsze'
+
+input.addEventListener('input', (event) => {
+  if (event.target.value === password)
+    div.textContent = message
+})
