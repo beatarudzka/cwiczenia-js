91cabab4bc4df3c67d4bbb798f3ec61eacc678f3
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-01 15:35:41 +0200

    forEach method

--- task11/script.js
+++ task11/script.js
@@ -1,11 +1,16 @@
 const input = document.querySelector('.password')
-const passwords = ['jeden', 'dwa']
+const passwords = ['JEden', 'dWa']
 const div = document.querySelector('div')
 const messages = ['11111', '22222']
 
+passwords.forEach((password, index) => {
+  passwords[index] = password.toLowerCase()
+
+})
 
 const showMessages = (event) => {
   const text = (event.target.value).toLowerCase()
+
   passwords.forEach((password, index) => {
     if (text === password) {
       div.textContent = messages[index]
