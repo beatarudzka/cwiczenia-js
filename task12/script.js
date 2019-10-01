bcaff29e762d997a0704b920cca349b9a7cdf25d
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-01 17:47:54 +0200

    refactoring

--- task12/script.js
+++ task12/script.js
@@ -2,7 +2,7 @@ const button = document.querySelector('button')
 const names = ["Gaja", "Dobrawa", "Jagienka", "Antosia", "Hanna", "Ola", "Kalina"]
 const div = document.querySelector('.showName')
 
-const indexOfName = Math.floor(Math.random() * 7)
+const indexOfName = Math.floor(Math.random() * names.length)
 
 const nameGenerator = () => {
   div.textContent = (names[indexOfName])
