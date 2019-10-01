e1e4927e5efbeff87d08672098d1c141224299e7
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-01 19:22:40 +0200

    adding names to div using button

--- task14/script.js
+++ task14/script.js
@@ -1,9 +1,14 @@
 const names = []
 
 const button = document.querySelector('button')
+const input = document.querySelector('input')
+const div = document.querySelector('div')
 
 const addName = (event) => {
   event.preventDefault()
-}
+  const name = input.value
+  names.push(name)
+  div.textContent += name + ','
 
+}
 button.addEventListener('click', addName)
