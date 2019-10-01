920466dc1a014c9eab350ce0df2b490093ee607c
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-01 20:41:49 +0200

    preventing adding empty ""

--- task14/script.js
+++ task14/script.js
@@ -6,9 +6,11 @@ const div = document.querySelector('div')
 
 const addName = (event) => {
   event.preventDefault()
-  const name = input.value
-  names.push(name)
-  div.textContent += name + ','
-
+  if (input.value.length) {
+    const name = input.value
+    names.push(name)
+    div.textContent += name + ','
+    input.value = ''
+  }
 }
 button.addEventListener('click', addName)
