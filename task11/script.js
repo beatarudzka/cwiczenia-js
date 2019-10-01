eff0f3a417715b7c82d3b36b7dabec1041ce8c0c
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-01 12:35:23 +0200

    function showMessages on the input

---
+++ task11/script.js
@@ -0,0 +1,9 @@
+const input = document.querySelector('.password')
+const passwords = ['JEden', 'dWa']
+const messages = ['11111', '22222']
+
+const showMessages = () => {
+  console.log('ok')
+}
+
+input.addEventListener('input', showMessages)
