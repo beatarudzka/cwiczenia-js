2f27ba851d1149ee15ca23b6094b02f58295637c
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-01 12:57:53 +0200

    password with toLowerCase function

--- task11/script.js
+++ task11/script.js
@@ -1,9 +1,18 @@
 const input = document.querySelector('.password')
-const passwords = ['JEden', 'dWa']
+const passwords = ['jeden', 'dwa']
+const div = document.querySelector('div')
 const messages = ['11111', '22222']
 
-const showMessages = () => {
-  console.log('ok')
+
+const showMessages = (event) => {
+  const text = (event.target.value).toLowerCase()
+  passwords.forEach((password, index) => {
+    if (text === password) {
+      div.textContent = messages[index]
+    } else {
+      div.textContent = ''
+    }
+  })
 }
 
 input.addEventListener('input', showMessages)
