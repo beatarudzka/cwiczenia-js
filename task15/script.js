53c183caaa88463c1e56a54f0ba9e7b52c2c7160
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-02 09:21:51 +0200

    function showAdvice after click on the button

--- task15/script.js
+++ task15/script.js
@@ -5,6 +5,7 @@ const showAllPossibilitiesButton = document.querySelector('.showAllPossibilities
 const div = document.querySelector('div')
 
 let possibilities = ['wyjdź na spacer z psem', 'wykonaj codzienny trening']
+let prefixes = []
 
 const addPossibility = (event) => {
   event.preventDefault()
@@ -18,5 +19,13 @@ const reset = (event) => {
   possibilities = []
   div.textContent = "Zresetowano wszystkie możliwości"
 }
+const showAdvise = (event) => {
+  event.preventDefault()
+  if (possibilities.length) {
+    const index = Math.floor(Math.random() * possibilities.length)
+    div.textContent = possibilities[index]
+  }
+}
 addPossibilityButton.addEventListener('click', addPossibility)
 resetButton.addEventListener('click', reset)
+showAdviseButton.addEventListener('click', showAdvise)
