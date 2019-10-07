a2b0b997798598753c4932cc4ee0895e75a620e8
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-07 21:28:05 +0200

    class  extends

--- task17/script.js
+++ task17/script.js
@@ -1,14 +1,24 @@
-class Country {
+class Person {
   constructor(name) {
     this.name = name
-    this.showName = () => console.log(this.name)
   }
-  showCountryName() {
-    console.log(`Nazwa kraju to ${this.name}.`)
+  showName() {
+    console.log(`Imię osoby to ${this.name}`)
   }
 }
 
-const Poland = new Country('Polska')
+class Student extends Person {
+  constructor(name = "", degrees = []) {
+    super(name)
+    this.degrees = degrees
+  }
+  showDegrees() {
+    const completed = this.degrees.filter(degree => degree > 2)
+    console.log(`Student ${this.name}, ma stopnie: ${this.degrees} i zaliczył już: ${completed.length} projektów`)
+  }
+}
 
-Poland.showCountryName()
-Poland.showName()
+const Marysia = new Student('Marysia', [2, 4, 5, 3, 4, 1, 2, 4, 5])
+
+Marysia.showName()
+Marysia.showDegrees()
