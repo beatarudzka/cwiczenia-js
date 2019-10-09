2f59409a9d42a2fdb9378898b797b1e4094553c5
Beata Rudzka <beatarudzka89@gmail.com>
2019-10-09 14:00:04 +0200

    bind first example

--- task17/script.js
+++ task17/script.js
@@ -1,24 +1,10 @@
-class Person {
-  constructor(name) {
-    this.name = name
-  }
+const dog = {
+  name: "rocky",
   showName() {
-    console.log(`Imię osoby to ${this.name}`)
+    console.log("imię psa to " + this.name)
   }
 }
 
-class Student extends Person {
-  constructor(name = "", degrees = []) {
-    super(name)
-    this.degrees = degrees
-  }
-  showDegrees() {
-    const completed = this.degrees.filter(degree => degree > 2)
-    console.log(`Student ${this.name}, ma stopnie: ${this.degrees} i zaliczył już: ${completed.length} projektów`)
-  }
-}
+dog.showName()
 
-const Marysia = new Student('Marysia', [2, 4, 5, 3, 4, 1, 2, 4, 5])
-
-Marysia.showName()
-Marysia.showDegrees()
+const dogName = dog.showName.bind(dog)
