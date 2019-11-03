cd768487e67577acb94c8539aa5974713c33170f
Beata Rudzka <beatarudzka89@gmail.com>
2019-11-03 19:11:43 +0100

    square digits

--- task21/script.js
+++ task21/script.js
@@ -1,29 +1,7 @@
-const fibonacci = {
-  getSequenceNumber: function (n) {
-
-    if (n === 0 || n === 1) {
-      return this.cache[n]
-    }
-    if (this.cache[n]) {
-      return this.cache[n]
-    }
-    else {
-      this.cache[n] = this.getSequenceNumber(n - 2) + this.getSequenceNumber(n - 1)
-    }
-
-    return this.cache[n]
-  },
-
-  cache: {
-    0: 0,
-    1: 1
+function squareDigits(num) {
+  const squareDigit = num.toString().split('')
+  for (let i = 0; i < squareDigit.length; i++) {
+    squareDigit[i] = Math.pow(squareDigit[i], 2)
   }
+  return parseInt(squareDigit.join(''))
 }
-
-console.log(fibonacci.getSequenceNumber(7))
-
-
-console.log(fibonacci.cache)
-
-
-console.log(Object.values(fibonacci.cache))
