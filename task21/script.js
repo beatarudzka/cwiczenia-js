09d38ae2614309e689b8211d5c9e700cae90fe66
Beata Rudzka <beatarudzka89@gmail.com>
2019-11-03 19:22:41 +0100

    squareDigit using map

--- task21/script.js
+++ task21/script.js
@@ -1,7 +1,23 @@
-function squareDigits(num) {
-  const squareDigit = num.toString().split('')
-  for (let i = 0; i < squareDigit.length; i++) {
-    squareDigit[i] = Math.pow(squareDigit[i], 2)
-  }
-  return parseInt(squareDigit.join(''))
+// function squareDigits(num) {
+//   const squareDigit = num.toString().split('')
+//   for (let i = 0; i < squareDigit.length; i++) {
+//     squareDigit[i] = Math.pow(squareDigit[i], 2)
+//   }
+//   return parseInt(squareDigit.join(''))
+// }
+
+// function squareDigits(num){
+//   let array = num.toString().split('').map( function(int) {
+//     let i = parseInt(int)
+//     return i * i
+//   })
+//   return parseInt(array.join(""))
+// }
+
+function squareDigit(num) {
+  let numArr = num.toString().split("").map(function (int) {
+    let indexArr = parseInt(int)
+    return Math.pow(indexArr, 2)
+  })
+  return parseInt(numArr.join(""))
 }
