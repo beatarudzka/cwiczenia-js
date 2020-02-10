d5e07383e1090c9dd532cd200ab2d61e464d3f0d
Beata Rudzka <beatarudzka89@gmail.com>
2020-02-10 18:17:56 +0100

    reduce and filter

--- task24/reduce.js
+++ task24/reduce.js
@@ -1,1 +1,14 @@
 const shoppingItems = require('./shoppingItems')
+
+
+const totalPrice = shoppingItems.reduce((acc, item) => {
+  return acc + item.price
+}, 0)
+console.log(totalPrice)
+
+
+const priceOfFood = shoppingItems.filter(item => {
+  return item.category === 'food'
+}).reduce((acc, food) => acc + food.price, 0)
+
+console.log(priceOfFood)
